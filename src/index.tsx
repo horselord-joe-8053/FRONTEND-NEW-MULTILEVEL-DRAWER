e50eee7527d85d2f27836b3836b3b4273a8b8826
horselord-joe-8053 <coder.joe.8053@gmail.com>
2022-04-15 21:40:03 +1000

    first commit, MUI responsive drawer

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import ReactDOM from 'react-dom/client';
+import { StyledEngineProvider } from '@mui/material/styles';
+import Demo from './demo';
+
+// jjw: https://stackoverflow.com/a/71808238 need to use
+// - '!' (non-null assertion) https://stackoverflow.com/a/42274019
+// OR - 'as Element'
+ReactDOM.createRoot(
+  document.querySelector('#root') as Element
+).render(
+  <StyledEngineProvider injectFirst>
+    <Demo />
+  </StyledEngineProvider>
+);
